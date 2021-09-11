50e1a802baeb425ba7000ca8e0e1cd9189ecbd43
NurAlif <nuralif.ilyasa@gmail.com>
2021-09-12 00:46:34 +0700

    Update op3_walking_module.h

--- pycontroller/op3_walking_module.h
+++ pycontroller/op3_walking_module.h
@@ -74,16 +74,16 @@ class WalkPID{
 
   WalkPID(){};
   WalkPID (double _p, double _i, double _d){
-    // std::chrono::milliseconds ms = std::chrono::duration_cast< std::chrono::milliseconds >(
-    //     std::chrono::system_clock::now().time_since_epoch()
-    // );
+    std::chrono::milliseconds ms = std::chrono::duration_cast< std::chrono::milliseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    );
 
-    sampleTime = 100;
+    sampleTime = 500;
     target = 0;
     output = 0;
     errorSum = 0;
     lastInput = 0;
-    // lastTime = ms.count() - sampleTime;
+    lastTime = ms.count() - sampleTime;
 
     setTunings(_p, _i, _d);
   };
@@ -112,21 +112,21 @@ class WalkPID{
   };
 
   double compute(double input){
-    // std::chrono::milliseconds now = std::chrono::duration_cast< std::chrono::milliseconds >(
-    //     std::chrono::system_clock::now().time_since_epoch()
-    // );
+    std::chrono::milliseconds now = std::chrono::duration_cast< std::chrono::milliseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    );
 
-    // double time_diff = now.count() - lastTime;
+    double time_diff = now.count() - lastTime;
 
-    // if(time_diff >= sampleTime){
+    if(time_diff >= sampleTime){
       double error = target - input;
       double inputDiff = input - lastInput;
 
       errorSum = std::max(min, std::min(max, errorSum + (i * error)));
       output = std::max(min, std::min(max, (p * error) + errorSum - (d * inputDiff)));
       lastInput = input;
-      // lastTime = now.count();
-    // }
+      lastTime = now.count();
+    }
 
     return output;
   }
